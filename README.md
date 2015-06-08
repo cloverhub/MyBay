0d50156118790ae264d826acdcde10197664a8df
Shawn Clover <git@shawnclover.com>
2015-06-07 21:41:54 -0700

    Fresh git init after google drive corruption issue

---
+++ README.md
@@ -0,0 +1,5 @@
+# Udacity FE Web Developer Project 5: Neighborhood Map
+
+## Additional Resources Used:
+- JavaScript Array.Push Performance (Scott Logic)
+- Stackoverflow
---
+++ js/model.js
@@ -0,0 +1,226 @@
+var locations = [
+	{
+		name: 'De Young (Museum)',
+		lat: 37.771250,
+		lng: -122.468597,
+		review: 'test',
+		wiki: '0',
+		category: ['museum']
+	},
+	{
+		name: 'Zero Zero',
+		lat: 37.781586,
+		lng: -122.401985,
+		review: 'Ordered nuvole pasta and a Market pizza along with caesar salad, drinks, and wine. The pasta was excellent as was the pizza. The pizzas are single serving-sized and a bit crispified underneath. The whole theme of the pizza style and vanilla ice cream thing are straight from Boot and Shoe Service in Oakland, but that&#39;s fine by me because I love that place too.</p><p>They seem to have a pizza management problem. We were expecting the pasta and the pizza to arrive at the same time, but instead we watched people sit down next to us after we arrived, order two pizzas, finish their pizzas and dessert and leave before our pizza even arrived. It was over an hour for one pizza.</p><p>Looking forward to repeat visits since I live across the street...just hope they can speed up the pizzas a bit.',
+		wiki: '0',
+		category: ['grub']
+	},
+	{
+		name: 'Le Charm',
+		lat: 37.780143,
+		lng: -122.402826,
+		review: 'If you blink you might miss this little gem tucked in behind the trees on the sidewalk. But walk in and you&#39;ll encounter wall-to-wall French decor, a towering ceiling with a huge chandelier, and a covered courtyard in back for semi-outdoor dining.</p><p>The French owners and the rest of the staff are outstanding and take the time to answer any of your questions in detail or to work with you on recommendations.</p><p>Every dish I&#39;ve tried is excellent. The pasta dishes are my personal favorite--these guys make pasta on par with the best Italian restaurants and pair it with delectable fall-off-the-bone meats. The pan-seared Salmon is really good too.</p><p>Don&#39;t come here in a hurry--working through a three-course meal can go on for hours in typical French fashion. Rather than a quiet atmosphere, you&#39;ll typically see a more lively (loud) crowd in here with upbeat (sometimes even Reggae) music in the air.',
+		wiki: '0',
+		category: ['grub']
+	},
+	{
+		name: 'Victors',
+		lat: 37.778690,
+		lng: -122.393275,
+		review: 'test',
+		wiki: '0',
+		category: ['grub']
+	},
+	{
+		name: 'Redwood Valley Railway',
+		lat: 37.880024,
+		lng:  -122.222198,
+		review: 'test',
+		wiki: '1',
+		category: ['fun']
+	},
+	{
+		name: 'Treasure Island, San Francisco',
+		lat: 37.822865,
+		lng: -122.370189,
+		review: 'test',
+		wiki: '0',
+		category: ['explore']
+	},
+	{
+		name: 'Pacific Pinball Museum',
+		lat: 37.773675,
+		lng: -122.276371,
+		review: 'test',
+		wiki: '1',
+		category: ['fun']
+	},
+	{
+		name: 'Golden Gate Bridge',
+		lat: 37.819713,
+		lng: -122.478437,
+		review: 'I&#39;ve walked over it more than 100 times, ridden my bike over it even more times, sailed under it, flown my plane over it, and have taken over 10,000 photos of it from every possible angle. This bridge is an Art Deco masterpiece, designed and built to absolute perfection. If you haven&#39;t done so, do yourself a favor and read a book about the history of the design (or take the shortcut and watch the Discovery Channel/PBS shows) and you&#39;ll appreciate the details of the bridge in a new light.</p><p>I&#39;ve been inside the Great Pyramid of Giza, to the top of the Eiffel Tower, and over the Hoover Dam, but without a doubt, the Golden Gate Bridge is the greatest man-made structure on the face of the earth.',
+		wiki: '1',
+		category: ['walk']
+	},
+	{
+		name: 'Musée Mécanique',
+		lat: 37.809267,
+		lng: -122.415957,
+		review: 'Testing Testing 1',
+		wiki: '1',
+		category: ['fun']
+	},
+	{
+		name: 'California Academy of Sciences',
+		lat: 37.769725,
+		lng: -122.4661,
+		review: 'Testing Testing 2',
+		wiki: '0',
+		category: ['museum']
+	},
+	{
+		name: 'Buchanan Field Airport',
+		lat: 37.988612,
+		lng: -122.055532,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['fun']
+	},
+	{
+		name: 'Sutro Baths',
+		lat: 37.780418,
+		lng: -122.513716,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['explore']
+	},
+	{
+		name: 'Zacharys Pizza',
+		lat: 37.846249,
+		lng: -122.252135,
+		review: 'Just realized I never reviewed Zachary&#39;s. What can I say? This is my favorite pizza place in the world...</p><p>Please bring Zachary&#39;s to San Francisco!!',
+		wiki: '1',
+		category: ['grub']
+	},
+	{
+		name: 'Computer History Museum',
+		lat: 37.413980,
+		lng: -122.077345,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['museum']
+	},
+	/*{
+		name: 'Sweeney Observatory Site',
+		lat: 37.768670,
+		lng: -122.475213,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['explore']
+	}, */
+	{
+		name: 'Stow Lake Boat House',
+		lat: 37.770203,
+		lng: -122.477412,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['workout']
+	},
+	{
+		name: 'Builders Booksource',
+		lat: 37.869503,
+		lng: -122.300330,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['gear']
+	},
+	{
+		name: 'Exploratorium',
+		lat: 37.801163,
+		lng: -122.398176,
+		review: 'I practically grew up in the Exploratorium. I didn&#39;t realize it when I was 7 years old, but I was already grasping the fundamentals of physics, biology, geology, thermodynamics, and botany. I could solve binary arithmetic problems and explain to my friends why a bicycle stands up. Things like computer programming and physics were a breeze later on in high school.</p><p>Although it&#39;s no longer donation-only, today&#39;s Exploratorium is better than ever. I&#39;ve had a family membership for years and bring the family in often. This is one of the best places around to bring kids--they will have fun and can&#39;t help but learn along the way. When the new location first opened, I missed the old dark, dank, cavernous feel of the original location, but the location, capacity, and overall atmosphere of the new location seems natural now. The food and waterfront view aren&#39;t bad either.</p><p>I&#39;ve been to hands-on science museums around the world that have followed the Exploratorium&#39;s model, but the Exploratorium is the original and still the best.',
+		wiki: '1',
+		category: ['museum']
+	},
+	{
+		name: 'Montclair Egg Shop',
+		lat: 37.826951,
+		lng: -122.210282,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['grub']
+	},
+	{
+		name: 'Grand Lake Theatre',
+		lat: 37.811452,
+		lng: -122.247520,
+		review: 'Choices:<br/></p><p>Live Wurlitzer organ performance or deafening Pepsi ads?</p><p>Good old popcorn or super-hydrogenated stuff soaked in yellow #7?</p><p>Museum-like classic projectors display in the lobby or video games?</p><p>Egyptian-themed theater or plain black paneled walls?</p><p>Awesome neo-classical interior or uninspiring dull endless hallways?</p><p>Polite moviegoers or obnoxious talking, texting morons?</p><p>$10 or $12?</p><p><br/>Why on earth would you choose a megaplex over the Grand?',
+		wiki: '1',
+		category: ['fun']
+	},
+	{
+		name: 'Hotel Utah Saloon',
+		lat: 37.779204,
+		lng: -122.397990,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['fun']
+	},
+	{
+		name: 'Little Star Pizza',
+		lat: 37.766353,
+		lng: -122.422405,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['grub']
+	},
+	{
+		name: 'Swensens Ice Cream',
+		lat: 37.799004,
+		lng: -122.419151,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['grub']
+	},
+	{
+		name: 'Angel Island (California)',
+		lat: 37.860747,
+		lng: -122.426567,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['hike']
+	},
+	{
+		name: 'Drawbridge, CA',
+		lat: 37.465879,
+		lng: -121.974506,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['explore']
+	},
+	{
+		name: 'Kezar Stadium',
+		lat: 37.767013,
+		lng: -122.456006,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['workout']
+	},
+	{
+		name: 'Fort Point',
+		lat: 37.810712, 
+		lng:-122.476916,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['explore']
+	},
+	{
+		name: 'Playland-Not-At-The-Beach',
+		lat: 37.916852,
+		lng: -122.312757,
+		review: 'Testing Testing 3',
+		wiki: '1',
+		category: ['fun']
+	}
+];
